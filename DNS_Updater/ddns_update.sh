43dee020a00ddb382d51062ef15a8042f37e6c2d
Barrett Lowe <barrett.lowe@etcconnect.com>
2020-01-13 22:58:39 -0600

    Bump version and fix - bashio still not working

--- DNS_Updater/ddns_update.sh
+++ DNS_Updater/ddns_update.sh
@@ -3,6 +3,7 @@
 # ./dynamic_dns_lambda_client.sh host1.dyn.example.com. SHARED_SECRET_1 "abc123.execute-api.us-west-2.amazonaws.com/prod"
 
 # If the script is called with no arguments, show an instructional error message.
+bashio::log.info "Running ddns_update.sh"
 if [ $# -eq 0 ]
     then
     bashio::log.error 'The script requires hostname and shared secret arguments.'
@@ -15,9 +16,10 @@ myHostname=$1
 mySharedSecret=$2
 myAPIURL=$3
 # Call the API in get mode to get the IP address
-myIP=`curl -q -s  "$myAPIURL?mode=get" | egrep -o '[0-9\.]+'`
+myIP=`curl -q -s  $myAPIURL?mode=get | egrep -o '[0-9\.]+'`
 # Build the hashed token
 myHash=`echo -n $myIP$myHostname$mySharedSecret | sha256sum | awk '{print $1}'`
 # Call the API in set mode to update Route 53
+bashio::log.info "Attempting to contact $myAPIURL"
 curl -q -s "$myAPIURL?mode=set&hostname=$myHostname&hash=$myHash"
 echo
--- DNS_Updater/entrypoint.sh
+++ DNS_Updater/entrypoint.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bashio
 
 # Start the run once job.
-echo "Cron Docker container has been started"
+bashio::log.info "Cron Docker container has been started"
 
 #declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
 
@@ -9,11 +9,14 @@ CONFIG_PATH=/data/options.json
 
 CRON_PATTERN=$(jq --raw-output ".cron_pattern" $CONFIG_PATH)
 
-echo "Cron Pattern: ${CRON_PATTERN}"
-echo "Config Path: ${CONFIG_PATH}"
+bashio::log.info "Cron Pattern: ${CRON_PATTERN}"
+bashio::log.info "Config Path: ${CONFIG_PATH}"
 
 # Setup a cron schedule
 echo "${CRON_PATTERN} /run.sh" >> /etc/crontabs/root
+
+bashio::log.info "Running run.sh from entrypoint"
+/run.sh
 
 crontab -l
 crond -m /dev/stdout -f
